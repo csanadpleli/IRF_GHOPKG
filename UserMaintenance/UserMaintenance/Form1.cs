bf5f066ab1dde0b8b0cd6713b3a4e05862756607
Csanad Pleli <csanad.pleli@studentambassadors.com>
2022-09-30 08:46:08 +0000

    Action set

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -12,20 +12,34 @@ using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
-    BindingList<User> users = new BindingList<User>();
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             lblLastName.Text = Resource1.LastName;
             lblFirstName.Text = Resource1.FirstName;
             btnAdd.Text = Resource1.Add;
+
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = txtLastName.Text,
+                FirstName = txtFirstName.Text
+            };
+            users.Add(u);
         }
     }
 }
