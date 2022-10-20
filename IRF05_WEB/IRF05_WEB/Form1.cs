18111ce823c9067bd51fda4fb18a0ecb12a2957d
Csanad Pleli <csanad.pleli@studentambassadors.com>
2022-10-20 21:38:44 +0000

    Xml

--- IRF05_WEB/IRF05_WEB/Form1.cs
+++ IRF05_WEB/IRF05_WEB/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IRF05_WEB.Entities;
+using IRF05_WEB.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +9,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace IRF05_WEB
 {
     public partial class Form1 : Form
     {
+
+        BindingList<RateData> Rates;
+
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates(request);
+
+            dataGridView1.DataSource = Rates;
+
+            XmlProcessing();
+        }
+
+        private void XmlProcessing()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date=DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                {
+                    rate.Value = value / unit;
+                }
+            }
+        }
+
+        private void GetExchangeRates(request)
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+
+            return response;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
