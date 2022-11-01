54790afee6174d02d3db0b10f3f4d7591f525f93
Csanad Pleli <csanad.pleli@studentambassadors.com>
2022-11-01 19:19:01 +0000

    Apply Abstraction

--- IRF06_WORKSHOP/IRF06_WORKSHOP/Form1.cs
+++ IRF06_WORKSHOP/IRF06_WORKSHOP/Form1.cs
@@ -1,4 +1,5 @@
-﻿using IRF06_WORKSHOP.Entities;
+﻿using IRF06_WORKSHOP.Abstraction;
+using IRF06_WORKSHOP.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,10 @@ namespace IRF06_WORKSHOP
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
+        private List<Toy> _toys = new List<Toy>();
 
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -36,7 +37,7 @@ namespace IRF06_WORKSHOP
         private void createTimer_Tick(object sender, EventArgs e)
         {
             var ball = Factory.CreateNew();
-            _balls.Add(ball);
+            _toys.Add(ball);
             ball.Left = -ball.Width;
             mainPanel.Controls.Add(ball);
         }
@@ -44,9 +45,9 @@ namespace IRF06_WORKSHOP
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPostition = 0;
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPostition)
                 {
                     maxPostition = ball.Left;
@@ -55,9 +56,9 @@ namespace IRF06_WORKSHOP
 
             if (maxPostition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
         }
     }
