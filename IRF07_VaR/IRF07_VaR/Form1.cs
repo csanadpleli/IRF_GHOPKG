b5b5ca6e37377fb9a36c653c8eb0bdb6b1d6dc27
Csanad Pleli <csanad.pleli@studentambassadors.com>
2022-11-10 21:14:25 +0000

    Dataset

--- IRF07_VaR/IRF07_VaR/Form1.cs
+++ IRF07_VaR/IRF07_VaR/Form1.cs
@@ -12,9 +12,20 @@ namespace IRF07_VaR
 {
     public partial class Form1 : Form
     {
+
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
