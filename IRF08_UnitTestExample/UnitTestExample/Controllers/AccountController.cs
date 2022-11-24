7060e8b2ca3d6319d3a24fb350f0a27b766d7f2e
Csanad Pleli <csanad.pleli@studentambassadors.com>
2022-11-24 23:17:18 +0000

    Test

--- IRF08_UnitTestExample/UnitTestExample/Controllers/AccountController.cs
+++ IRF08_UnitTestExample/UnitTestExample/Controllers/AccountController.cs
@@ -25,7 +25,7 @@ namespace UnitTestExample.Controllers
             if(!ValidateEmail(email))
                 throw new ValidationException(
                     "A megadott e-mail cím nem megfelelő!");
-            if(!ValidateEmail(email))
+            if(!ValidatePassword(password))
                 throw new ValidationException(
                     "A megadottt jelszó nem megfelelő!\n" +
                     "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
@@ -50,7 +50,36 @@ namespace UnitTestExample.Controllers
 
         public bool ValidatePassword(string password)
         {
-            return true;
+            if (Regex.IsMatch(password, @"[A-Za-z0-9]{8,}")) 
+            {
+                if (Regex.IsMatch(password, @"[A-Z]")) 
+                {
+                    if (Regex.IsMatch(password, @"[a-z]")) 
+                    {
+                        if (Regex.IsMatch(password, @"[0-9]")) 
+                        {
+                            return true; 
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
